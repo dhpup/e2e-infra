3f66f1b252eb36dd223513c3f264316e90fc62bd
Daniel Hoang <danielvanhoang@gmail.com>
2026-05-02 21:24:01 -0500

    fix(infra): add health status customizations for Application resource in argocd_cm

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -12,6 +12,20 @@ resource "akp_instance" "argocd" {
   }
   argocd_cm = {
     "accounts.admin" = "login"
+    "resource.customizations.health.argoproj.io_Application" = <<-EOT
+      hs = {}
+      hs.status = "Progressing"
+      hs.message = ""
+      if obj.status ~= nil then
+        if obj.status.health ~= nil then
+          hs.status = obj.status.health.status
+          if obj.status.health.message ~= nil then
+            hs.message = obj.status.health.message
+          end
+        end
+      end
+      return hs
+    EOT
     "resource.customizations.actions.argoproj.io_ApplicationSet" = <<-EOT
       discovery.lua: |
         actions = {}
